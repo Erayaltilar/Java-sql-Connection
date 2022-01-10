a33a5d7a3f5935ff000d189d237d27b728fee411
Eray Altılar <74265465+Erayaltilar@users.noreply.github.com>
2022-01-10 13:34:40 +0300

    Add files via upload

---
+++ teacherConnection.java
@@ -0,0 +1,23 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class teacherConnection {
+	static ResultSet yap() {
+		ResultSet myRs = null;
+		String url = "jdbc:sqlserver://DESKTOP-IB35G0J\\MSSQLSERVER;databaseName=YabanciDilKursu";
+		String user = "sqlserveray";
+		String password = "nomad344";
+		try {
+			Connection connection = DriverManager.getConnection(url, user, password);
+			System.out.println("Veri yüklendi !");
+			Statement statement = connection.createStatement();
+			myRs = statement.executeQuery("select * from ogretmenler");
+		} catch (Exception e) {
+			System.out.println("Oops,Bir sorun var !");
+			e.printStackTrace();
+		}
+		return myRs;
+	}
+}
