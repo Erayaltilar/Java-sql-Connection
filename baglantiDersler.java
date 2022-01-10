5d856303342a921df3a474f7fe60afa5ed874372
Eray Altılar <74265465+Erayaltilar@users.noreply.github.com>
2022-01-10 13:29:25 +0300

    Add files via upload

---
+++ baglantiDersler.java
@@ -0,0 +1,22 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class baglantiDersler {
+	static ResultSet yap() {
+		ResultSet myRs = null;
+		String url = "jdbc:sqlserver://DESKTOP-IB35G0J\\MSSQLSERVER;databaseName=YabanciDilKursu";
+		String user = "sqlserveray";
+		String password = "nomad344";
+		try {
+			Connection connection = DriverManager.getConnection(url, user, password);
+			System.out.println("Data Imported !");
+			Statement statement = connection.createStatement();
+			myRs = statement.executeQuery("select * from dersler");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return myRs;
+	}
+}
